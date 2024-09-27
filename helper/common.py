afc16b14bb1442c4edd0f7c46acc29f218f4f36c
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2024-09-27 20:37:27 +0800

    update common parser

--- helper/common.py
+++ helper/common.py
@@ -5,4 +5,6 @@ class CommonParser:
     __ignored_fields = ["validator"]
 
     def parse_fields(self, obj):
+        if obj is None:
+            return []
         return list(filter(lambda x: not x.startswith("_") and x not in self.__ignored_fields  and not callable(getattr(obj.__class__, x)), dir(obj)))
